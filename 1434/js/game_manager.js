31715e94c75e24519f794059158372ff1e4b8b24
Daniel Ge <shurongge@gmail.com>
2023-06-02 18:08:57 -0700

    s

--- 1434/js/game_manager.js
+++ 1434/js/game_manager.js
@@ -79,7 +79,7 @@ GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
     var value;
     if(document.querySelector('.allow0').checked) value = Math.random() < 0.9 ? 5 : 0;
-    else value = Math.random() < 0.9 ? 1 : 2;
+    else value = Math.random() < 0.9 ? 1434 : 2;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
